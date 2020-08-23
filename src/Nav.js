198a182c14a76d578c6d315a463d3452286c61b7
rabobahago <rabobahago@gmail.com>
2020-08-23 15:24:56 +0100

    link added to the nav bar

--- src/Nav.js
+++ src/Nav.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Nav() {
+  const navStyle = {
+    color: 'white'
+  }
   return (
     <nav>
         <h2>Logo</h2>
         <ul className="nav-links">
-            <li>About</li>
-            <li>Shop</li>
+            <Link to='/about' style={navStyle}>
+              <li>About</li>
+            </Link>
+            <Link to='/shop' style={navStyle}>
+              <li>Shop</li>
+            </Link>
         </ul>
     </nav>
   );
