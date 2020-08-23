e6da1e4f0f2140f9f587901a4aab1d95c44176e9
rabobahago <rabobahago@gmail.com>
2020-08-23 17:04:01 +0100

    api data added

--- src/Nav.js
+++ src/Nav.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
 function Nav() {
   const navStyle = {
     color: 'white'
--- src/Shop.js
+++ src/Shop.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Shop() {
+  const [users, setUsers] = useState([]);
+  useEffect(()=>{
+    fetchItems();
+  }, []);
+  const fetchItems = async ()=>{
+  const data = await fetch('https://jsonplaceholder.typicode.com/users')
+   const items = await data.json();
+   setUsers(items);
+  }
   return (
     <div className="App">
-      <h2>Shop Page</h2>
+      {
+        users.map(user=>(
+          <div key={user.id}>
+            <h2>Name:{user.name}</h2>
+            <p>{user.username}</p>
+            <p>{user.email}</p>
+            <br/>
+          </div>
+        ))
+      }
     </div>
   );
 }
-
 export default Shop;
